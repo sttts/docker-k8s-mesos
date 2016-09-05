5a65bd87ad259b58303459263148c1836bc38398
Sebastian Scheele <sebastian@loodse.com>
2016-09-05 17:17:45 +0200

    change to production mode

--- config/config.js
+++ config/config.js
@@ -11,27 +11,6 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-       storage: {
-          active: 'ghost-cloudinary-store'
-       },
-        url: 'http://my-ghost-blog.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost.db')
-            },
-            debug: false
-        },
-
-        server: {
-            host: '0.0.0.0',
-            port: '2368'
-        }
-    },
-
-    // ### Development **(default)**
-    development: {
         storage: {
             active: 'ghost-cloudinary-store'
         },
@@ -80,80 +59,6 @@ config = {
         paths: {
             contentPath: path.join(process.env.GHOST_CONTENT, '/')
         }
-    },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    testing: {
-       storage: {
-          active: 'ghost-cloudinary-store'
-       },
-
-        url: 'http://0.0.0.0:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-test.db')
-            },
-            pool: {
-                afterCreate: function (conn, done) {
-                    conn.run('PRAGMA synchronous=OFF;' +
-                    'PRAGMA journal_mode=MEMORY;' +
-                    'PRAGMA locking_mode=EXCLUSIVE;' +
-                    'BEGIN EXCLUSIVE; COMMIT;', done);
-                }
-            }
-        },
-        server: {
-            host: '0.0.0.0',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
-    'testing-mysql': {
-        url: 'http://0.0.0.0:2369',
-        database: {
-            client: 'mysql',
-            connection: {
-                host     : '0.0.0.0',
-                user     : 'root',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '0.0.0.0',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing pg
-    // Used by Travis - Automated testing run through GitHub
-    'testing-pg': {
-        url: 'http://0.0.0.0:2369',
-        database: {
-            client: 'pg',
-            connection: {
-                host     : '0.0.0.0',
-                user     : 'postgres',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '0.0.0.0',
-            port: '2369'
-        },
-        logging: false
     }
 };
 
